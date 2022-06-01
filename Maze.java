c330bf75cb686153015cf3f611e3b37aa52bbbbc
detaomega <cosmolynnyang@gmail.com>
2022-06-01 23:56:09 +0800

    feat: double score

--- Maze.java
+++ Maze.java
@@ -7,10 +7,14 @@ import javax.swing.*;
 public class Maze {
 
     private Color dotColor = new Color(192, 192, 0);
+    private Color player1Color = new Color(255, 254, 56);
+    private Color player2Color = new Color(0, 255, 0);
     private Color mazeColor = new Color(5, 100, 5);
     private int BLOCK_SIZE = 24, N_BLOCKS;
     public int []data = new int[30 * 30];
-    private int count = 0;
+    private int count;
+    private int doubleTime = 0;
+    public int player;
     public boolean checkMaze() {
         for (int i = 0; i < N_BLOCKS * N_BLOCKS; i++) {
             if ((data[i] & 48) != 0) {
@@ -23,6 +27,9 @@ public class Maze {
 
     public Maze(int N_BLOCKS) {
         this.N_BLOCKS = N_BLOCKS;
+        player = 0;
+        count = 0;
+        doubleTime = 0;
     }
 
     public void drawMaze(Graphics2D g2d) {
@@ -51,8 +58,18 @@ public class Maze {
                 }
 
                 if ((data[i] & 16) != 0) { // 果實
-                    g2d.setColor(dotColor);
-                    g2d.fillRect(x + 11, y + 11, 2, 2);
+                    if (player == 1)
+                        g2d.setColor(player1Color);
+                    else 
+                        g2d.setColor(player2Color);
+                    if (doubleTime > 0) {
+                        if (doubleTime >= 30 || doubleTime % 6 <= 2)
+                            g2d.fillOval(x + 9, y + 9, 6, 6);
+                    }
+                    else {
+                        g2d.setColor(dotColor);
+                        g2d.fillRect(x + 11, y + 11, 2, 2);
+                    }
                 }
 
                 if ((data[i] & 32) != 0 && count % 6 <= 2) {
@@ -62,6 +79,16 @@ public class Maze {
                 i++;
             }
         }
+        if (doubleTime > 0) {
+            doubleTime--;
+        }
+        if (doubleTime == 0) player = 0;
+        
         count++;
+    }
+
+    public void doubleScore(int PLAYER) {
+        doubleTime = 150;
+        player = PLAYER;
     }
 }
