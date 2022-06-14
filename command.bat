b0ead127267c2f04b09618ccc2811528672ebd9d
detaomega <cosmolynnyang@gmail.com>
2022-06-14 20:00:17 +0800

    fix bat file

--- command.bat
+++ command.bat
@@ -1,4 +1,4 @@
 set PATH_TO_FX="C:\ProgramFiles\Java\javafx-sdk-17.0.2\lib"
-javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml *.java
-java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml Pacman
+javac --module-path "%PATH_TO_FX%" --add-modules javafx.controls,javafx.fxml *.java
+java --module-path "%PATH_TO_FX%" --add-modules javafx.controls,javafx.fxml Pacman
 pause
