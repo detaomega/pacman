28fce391c5697c1ceff9bfc838aeb0d9234ba9da
detaomega <cosmolynnyang@gmail.com>
2022-06-14 19:38:17 +0800

    update window batch file

--- command.bat
+++ command.bat
@@ -1,3 +1,4 @@
-javac --module-path $env:PATH_TO_FX --add-modules=javafx.controls,javafx.fxml *.java
-java --module-path $env:PATH_TO_FX --add-modules=javafx.controls,javafx.fxml Pacman
+set PATH_TO_FX="C:\ProgramFiles\Java\javafx-sdk-17.0.2\lib"
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml *.java
+java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml Pacman
 pause
